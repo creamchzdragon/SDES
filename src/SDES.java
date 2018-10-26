ed275056e31bdcbc56587d910c1915e2966b3d8f
Tyler W <jtwalder@gmail.com>
2018-10-26 11:27:30 -0400

    completed S blocks

--- src/SDES.java
+++ src/SDES.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
@@ -9,7 +11,52 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class SDES {
 	private final static int[] EP= {3,0,1,2,1,2,3,0};
 	private final static int[] P4= {1,3,2,0};
-	public SDES() {}
+	//boolean arrays when turn into bytes are equivalent to one of these sixteen arrays ie [0,0,0,0] is 0000 in bytes and 0 in base ten and thus position 0 in our table
+	//s0
+	//0000:01,0001:11,0010:00,0011:10,0100:11,0101:01,0110:10,0111:00
+	//1000:00,1001:11,1010:10,1011:01,1100:01,1101:11,1110:11,1111:10
+	private final static boolean[][] s0= {
+			{false,true},//0000
+			{true,true},//0001
+			{false,false},//0010
+			{true,false},//0011
+			{true,true},//0100
+			{false,true},//0101
+			{true,false},//0110
+			{false,false},//0111
+			{false,false},//1000
+			{true,true},//1001
+			{true,false},//1010
+			{false,true},//1011
+			{false,true},//1100
+			{true,true},//1101
+			{true,true},//1110
+			{true,false}//1111
+	};
+	//boolean arrays when turn into bytes are equivalent to one of these sixteen arrays ie [0,0,0,0] is 0000 in bytes and 0 in base ten and thus position 0 in our table
+		//s1
+		//0000:00,0001:10,0010:01,0011:00,0100:10,0101:01,0110:11,0111:11
+		//1000:11,1001:10,1010:00,1011:01,1100:01,1101:00,1110:00,1111:11
+		private final static boolean[][] s1= {
+				{false,false},//0000
+				{true,false},//0001
+				{false,true},//0010
+				{false,false},//0011
+				{true,false},//0100
+				{false,true},//0101
+				{true,true},//0110
+				{true,true},//0111
+				{true,true},//1000
+				{true,false},//1001
+				{false,false},//1010
+				{false,true},//1011
+				{false,true},//1100
+				{false,false},//1101
+				{false,false},//1110
+				{true,true}//1111
+		};
+	public SDES() {
+	}
 	/***
 	 * Convert the given bit array to a single byte
 	 * @param inp A bit array, max length is 8 bits
@@ -92,7 +139,7 @@ public class SDES {
 	}
 	/***
 	 * This is the 'round' function. It is its own inverse. f(x,k) = (L(x) xor F(R(x), k)) || R(x)
-
+	 *@author Jamie Walder
 	 * @param x input message
 	 * @param k key 
 	 * @return result of f operation
@@ -101,6 +148,7 @@ public class SDES {
 		return concat(xor(lh(x),feistel(rh(x),k)),rh(x));
 	}
 	/***
+	 * @author Jamie Walder
 	 * F(k,x) is a Feistel function F(k,x) = P4 (s0 (L (k xor EP(x))) || s1 (R (k xor EP(x)))
 	 * @param k key to encrypt with 
 	 * @param x message to encrypt
@@ -161,7 +209,7 @@ public class SDES {
 	 * @return two bit output of sblock
 	 */
 	public boolean[] s0(boolean[] input) {
-		throw new NotImplementedException();
+		return this.s0[bitArrayToByte(input)];
 	}
 	/***
 	 * s1 function 
@@ -169,7 +217,8 @@ public class SDES {
 	 * @return two bit output of sblock
 	 */
 	public boolean[] s1(boolean[] input) {
-		throw new NotImplementedException();
+		return this.s1[bitArrayToByte(input)];
+		
 	}
 			
 	
