d2572152bbdccd4ba537edf4438cba41071bd2e6
smitsd0 <43766738+smitsd0@users.noreply.github.com>
2018-11-07 21:56:58 -0500

    Add files via upload

--- src/SDES.java
+++ src/SDES.java
@@ -1,18 +1,24 @@
+package sdes;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 /***
  * 
- * @author Jamie Walder, Matthew Mallon,
+ * @author Jamie Walder, Matthew Mallon, David Smits
  *
  */
 public class SDES {
 	private final static int[] EP= {3,0,1,2,1,2,3,0};
 	private final static int[] P4= {1,3,2,0};
+	private final static int[] InitPerm= {1,5,2,0,3,7,4,6};
+	private final static int[] InvInitPerm= {3,0,2,4,6,1,7,5};
+	private final static int[] K1Perm= {0,1,0,1,1,1,1,1};
+	private final static int[] K2Perm= {1,1,1,1,1,1,0,0};
 	public boolean[] key = new boolean[10];
-	//boolean arrays when turned into bytes are equivalent to one of these sixteen arrays ie [0,0,0,0] is 0000 in bytes and 0 in base ten and thus position 0 in our table
+	//boolean arrays when turned into bytes are equivalent to one of these sixteen arrays ie [0,0,0,0] is 0000 in 
+	//bytes and 0 in base ten and thus position 0 in our table
 	//s0
 	//0000:01,0001:11,0010:00,0011:10,0100:11,0101:01,0110:10,0111:00
 	//1000:00,1001:11,1010:10,1011:01,1100:01,1101:11,1110:11,1111:10
@@ -34,7 +40,8 @@ public class SDES {
 			{true,true},//1110
 			{true,false}//1111
 	};
-	//boolean arrays whened turn into bytes are equivalent to one of these sixteen arrays ie [0,0,0,0] is 0000 in bytes and 0 in base ten and thus position 0 in our table
+	//boolean arrays whened turn into bytes are equivalent to one of these sixteen arrays ie [0,0,0,0] is 0000 in 
+	//bytes and 0 in base ten and thus position 0 in our table
 		//s1
 		//0000:00,0001:10,0010:01,0011:00,0100:10,0101:01,0110:11,0111:11
 		//1000:11,1001:10,1010:00,1011:01,1100:01,1101:00,1110:00,1111:11
@@ -118,38 +125,96 @@ public class SDES {
 		}
 		return concat;
 	}
+	
 	/***
-	 * Decrypt the given byte array.
-	 * @param cipher An array of bytes representing the cipher text.
-	 * @return An array of bytes representing the original plain text.
-	 */
-	public byte[] decrypt(byte[] cipher) {
-		throw new NotImplementedException();
-	}
-	/***
-	 * Decrypt a single byte using SDES
-	 * @param b the byte to be encyrpted
-	 * @return The decrypted byte
-	 */
-	public byte decryptByte(byte b) {
-		throw new NotImplementedException();
-	}
-	/***
-	 *  Encrypt the given string using SDES Each character produces a byte of cipher.
+	 * @author David Smits
+	 * Encrypt the given string using SDES Each character produces a byte of cipher.
 	 * @param msg The message to be encrypted
 	 * @return An array of bytes representing the cipher text.
 	 */
-	public byte[] encrypt(String msg) {
-		throw new NotImplementedException();
+	public byte[] encrypt(String plain) {
+		byte[] plainBytes = plain.getBytes();
+		byte[] result = new byte[plainBytes.length];
+		
+		
+		for(int i = 0; i < plainBytes.length; i++)
+			result[i] = encryptByte(plainBytes[i]);
+		
+		return result;
 	}
+	
 	/***
+	 * @author David Smits
 	 * Encrypt a single byte using SDES
 	 * @param b The byte to be encrypted
 	 * @return The encrypted byte
 	 */
 	public byte encryptByte(byte b) {
-		throw new NotImplementedException();
+		boolean[] bitArray = byteToBitArray(b, 10);
+		boolean[] k1 = expPerm(key, K1Perm);
+		boolean[] k2 = expPerm(key, K2Perm);
+		
+		boolean[] IP = expPerm(bitArray, InitPerm);
+		boolean[] fk1 = concat(xor(lh(IP), feistel(k1, rh(IP))), rh(IP));
+		boolean[] swapfk1 = concat(rh(fk1), lh(fk1));
+		boolean[] fk2 = concat(xor(lh(swapfk1), feistel(k2, rh(swapfk1))), rh(swapfk1));
+		boolean[] cipher = expPerm(fk2, InvInitPerm);
+		
+		return bitArrayToByte(cipher);
 	}
+	
+	/***
+	 * @author David Smits
+	 * Decrypt the given byte array.
+	 * @param cipher An array of bytes representing the cipher text.
+	 * @return An array of bytes representing the original plain text.
+	 */
+	public byte[] decrypt(byte[] cipher) {
+		byte[] result = new byte[cipher.length];
+		
+		for(int i = 0; i < cipher.length; i++)
+			result[i] = decryptByte(cipher[i]);
+		return result;
+	}
+	
+	/***
+	 * @author David Smits
+	 * Decrypt a single byte using SDES
+	 * @param b the byte to be encyrpted
+	 * @return The decrypted byte
+	 */
+	public byte decryptByte(byte b) {
+		boolean[] arrayByte = byteToBitArray(b, 0);
+		boolean[] k1 = expPerm(key, K1Perm);
+		boolean[] k2 = expPerm(key, K2Perm);
+		
+		boolean[] IP = expPerm(arrayByte, InitPerm);
+		boolean[] inversefk2 = concat(xor(lh(IP), feistel(k2, rh(IP))), rh(IP));
+		boolean[] swapfk2 = concat(rh(inversefk2), lh(inversefk2));
+		boolean[] fk1 = concat(xor(lh(swapfk2), feistel(k1, rh(swapfk2))), rh(swapfk2));
+		boolean[] plain = expPerm(fk1, InvInitPerm);
+		
+		return bitArrayToByte(plain);
+	}
+	
+	/***
+	 * @author David Smits
+	 * Send the bitArray to stdout as 1's and 0's
+	 * @param inp input
+	 */
+	public void show(boolean[] inp) {
+		System.out.println(bitArrayToByte(inp));
+	}
+	
+	/***
+	 * @author David Smits
+	 * Send the byteArray to stdout
+	 * @param byteArray input
+	 */
+	public void show(byte[] byteArray) {
+		System.out.println(byteArrayToString(byteArray));
+	}
+	
 	/***
 	 * @author Matthew Mallon
 	 * Expand and/or permute and/or select from the bit array, 
@@ -247,20 +312,6 @@ public class SDES {
 			rh[i] = inp[i + size];
 		}
 		return rh;
-	}
-	/***
-	 * Send the bitArray to stdout as 1's and 0's
-	 * @param inp input
-	 */
-	public void show(boolean[] inp) {
-		throw new NotImplementedException();
-	}
-	/***
-	 * Send the byteArray to stdout
-	 * @param byteArray input
-	 */
-	public void show(byte[] byteArray) {
-		throw new NotImplementedException();
 	}
 	/***
 	 * Exclusive OR. x and y must have the same length. x xor y is the same as x != y
